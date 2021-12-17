69cba78268aa9cc8851194f65870e53ded2607f4
Rahul Umap <umaprahul26@gmail.com>
2021-12-17 16:08:06 +0530

    - Search matrix

--- Day 4/Leetcode-problem-74.playground/Contents.swift
+++ Day 4/Leetcode-problem-74.playground/Contents.swift
@@ -15,5 +15,36 @@ func searchMatrixNotOptimised(_ matrix: [[Int]], _ target: Int) -> Bool {
     return false
 }
 
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    guard !matrix.isEmpty else { return false }
+    
+    let column = matrix.first!.count
+    let row = matrix.count
+    
+    var left = 0
+    var right = column * row - 1
+
+    while left <= right {
+        let midPoint = left + (right-left) / 2
+        
+        let resultRow = midPoint / column
+        let resultColumn = midPoint % column
+        
+        let midElement = matrix[resultRow][resultColumn]
+                
+        if midElement == target {
+            return true
+        } else if midElement < target {
+            left = midPoint + 1
+        } else if midElement > target {
+            right = midPoint - 1
+        }
+        
+    }
+    
+    return false
+}
+
+
 let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-searchMatrixNotOptimised(matrix, 11)
+searchMatrix(matrix, 3)
