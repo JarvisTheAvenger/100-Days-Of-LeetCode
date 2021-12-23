a6c1c2c55e412f36b3d8732525df85309288ac85
Rahul Umap <umaprahul26@gmail.com>
2021-12-23 18:16:59 +0530

    - improved answer

--- Day 10/GFG-MaximumProductSubarray.playground/Contents.swift
+++ Day 10/GFG-MaximumProductSubarray.playground/Contents.swift
@@ -1,18 +1,33 @@
 import Foundation
 
 // https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1
+// https://leetcode.com/problems/maximum-product-subarray/
 
-func maximumProductSubArray(_ array: [Int]) -> Int {
-    var maximum = array.first!
+func maxProduct(_ nums: [Int]) -> Int {
+    var maximum = nums.first!
     var product = 1
     
-    for element in array {
-        if product == 0 { product = 1 }
+    for element in nums {
         product *= element
         maximum = max(product, maximum)
+                
+        if product == 0 { product = 1 }
+    }
+    
+    product = 1
+    
+    for element in nums.reversed() {
+        product *= element
+        maximum = max(product, maximum)
+        
+        if product == 0 { product = 1 }
     }
     
     return maximum
 }
 
-maximumProductSubArray([2, 3, 4, 5, -1, 0])
+// -2,0,-1
+// 3,-1,4 --> 4
+// -3, -1, -4
+maxProduct([-3,-1,-4])
+
