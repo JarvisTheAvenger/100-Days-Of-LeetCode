2679bf45dc7aa63cac539a1cc0fa85bd8b1b4b05
Rahul Umap <umaprahul26@gmail.com>
2021-12-25 21:01:35 +0530

    Find missing number

---
+++ Day 12/Leetcode-problem-268.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+// https://leetcode.com/problems/missing-number/
+
+func missingNumber(_ nums: [Int]) -> Int {
+    let sortedNums = nums.sorted()
+    
+    var count = 0
+    
+    for index in 0...sortedNums.count {
+        
+        if index > sortedNums.count-1 {
+            return index
+        }
+        
+        if sortedNums[index] != count {
+            return count
+        }
+        
+        count += 1
+    }
+    
+    return -1
+}
+
+missingNumber([9,6,4,2,3,5,7,0,1])
