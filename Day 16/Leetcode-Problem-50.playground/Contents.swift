045bd3ef296a57431ccf3623605df78c73f3df84
Rahul Umap <umaprahul26@gmail.com>
2021-12-29 08:31:10 +0530

    implement power function

---
+++ Day 16/Leetcode-Problem-50.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// https://leetcode.com/problems/powx-n/
+
+func myPow(_ x: Double, _ n: Int) -> Double {
+    if n == 0 || x == 1 {
+        return 1
+    }
+    
+    if x == -1 && n % 2 != 0 {
+        return -1
+    }
+    
+    if x == -1 && n % 2 == 0 {
+        return 1
+    }
+
+    var product = 1.0
+
+    for _ in 1...n.magnitude {
+        product *= x
+    }
+
+
+    if n.signum() == -1 {
+        return 1 / product
+    }
+
+    return product
+}
+
+myPow(1.00000, -2147483648)
